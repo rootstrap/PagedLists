b0ae5e8e0d9ee2498fa75a87aa9718a74586966e
German Lopez <german@rootstrap.com>
2020-06-11 13:10:39 -0300

    Add library project for Carthage shared scheme

---
+++ Supporting Files/PagedLists.h
@@ -0,0 +1,19 @@
+//
+//  PagedLists.h
+//  PagedLists
+//
+//  Created by German on 6/11/20.
+//  Copyright © 2020 Rootstrap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PagedLists.
+FOUNDATION_EXPORT double PagedListsVersionNumber;
+
+//! Project version string for PagedLists.
+FOUNDATION_EXPORT const unsigned char PagedListsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagedLists/PublicHeader.h>
+
+
